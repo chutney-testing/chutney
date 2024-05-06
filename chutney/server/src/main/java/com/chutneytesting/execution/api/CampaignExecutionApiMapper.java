e481e3070b8e94c70ad8f8834222ad15581def78
DelaunayAlex <delaunayalexandre9518@gmail.com>
2024-05-06 09:27:15 +0200

    fix() : fix mapper mapstruct

--- chutney/server/src/main/java/com/chutneytesting/execution/api/CampaignExecutionApiMapper.java
+++ chutney/server/src/main/java/com/chutneytesting/execution/api/CampaignExecutionApiMapper.java
@@ -16,18 +16,25 @@
 
 package com.chutneytesting.execution.api;
 
+import com.chutneytesting.server.core.domain.execution.report.ServerReportStatus;
 import com.chutneytesting.server.core.domain.scenario.campaign.CampaignExecution;
 import java.util.Optional;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface CampaignExecutionApiMapper {
-    @Mapping(expression = "java( campaignExecution.status() )", target = "status")
+    @Mapping(target = "status", source = ".", qualifiedByName = "mapStatus")
     CampaignExecutionReportSummaryDto toCampaignExecutionReportSummaryDto(CampaignExecution campaignExecution);
 
     default String mapOptionalString(Optional<String> value) {
         return value.orElse(null);
+    }
+
+    @Named("mapStatus")
+    default ServerReportStatus mapStatus(CampaignExecution campaignExecution) {
+        return campaignExecution.status();
     }
 }
