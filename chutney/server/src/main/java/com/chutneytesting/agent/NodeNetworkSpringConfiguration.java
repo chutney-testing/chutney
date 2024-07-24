f116f080e3334341dfdb5e99b994c9988e00ebc2
DelaunayAlex <delaunayalexandre9518@gmail.com>
2024-07-24 09:11:37 +0200

    fix(server, engine): Remove unused import

--- chutney/server/src/main/java/com/chutneytesting/agent/NodeNetworkSpringConfiguration.java
+++ chutney/server/src/main/java/com/chutneytesting/agent/NodeNetworkSpringConfiguration.java
@@ -31,7 +31,6 @@ import com.chutneytesting.agent.domain.explore.ExploreAgentsService;
 import com.chutneytesting.agent.infra.storage.JsonFileAgentNetworkDao;
 import com.chutneytesting.engine.domain.delegation.ConnectionChecker;
 import com.chutneytesting.environment.api.environment.EmbeddedEnvironmentApi;
-import com.chutneytesting.tools.MyMixInForIgnoreType;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
