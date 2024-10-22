3e5e15b093cae9865d290718f7e4a20426278327
Alexandre Delaunay <delaunayalexandre9518@gmail.com>
2024-10-22 08:18:26 +0200

    feat(server, ui): Add licence

--- chutney/ui/src/app/core/services/sso.service.ts
+++ chutney/ui/src/app/core/services/sso.service.ts
@@ -1,3 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2024 Enedis
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ */
+
 import { HttpClient } from '@angular/common/http';
 import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
 import { environment } from '@env/environment';
