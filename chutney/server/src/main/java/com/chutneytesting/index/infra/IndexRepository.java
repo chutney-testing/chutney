9b40b79d16f7efa5b53d413837cec0cb648346c0
Goubbaa Karim <karim.goubaa@gmail.com>
2024-10-24 14:16:50 +0200

    chore(): index & search sorted

--- chutney/server/src/main/java/com/chutneytesting/index/infra/IndexRepository.java
+++ chutney/server/src/main/java/com/chutneytesting/index/infra/IndexRepository.java
@@ -5,16 +5,16 @@
  *
  */
 
-package com.chutneytesting.index.domain;
+package com.chutneytesting.index.infra;
 
 import java.util.List;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.Sort;
 
 public interface IndexRepository {
 
     void index(Document document);
-    List<Document> search(Query query, int limit);
-
+    List<Document> search(Query query, int limit, Sort sort);
     void delete(Query query);
 }
